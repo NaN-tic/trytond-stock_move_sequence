79fd196c97f42d9f1f41a4da627b483651f485f3
resteve <resteve@zikzakmedia.com>
2015-12-01 11:25:02 +0100

    Tests

--- tests/test_stock_move_sequence.py
+++ tests/test_stock_move_sequence.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the stock_move_sequence module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockMoveSequenceTestCase(unittest.TestCase):
+class StockMoveSequenceTestCase(ModuleTestCase):
     'Test Stock Move Sequence module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_move_sequence')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_move_sequence')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_move_sequence'
 
 
 def suite():
