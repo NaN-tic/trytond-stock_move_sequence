3f9ef0172d1ff07e17e618d98f05c1d88e837b88
Raimon Esteve <raimon@nan-tic.com>
2018-05-24 00:25:17 +0200

    Upgrade to 4.8

--- __init__.py
+++ __init__.py
@@ -2,18 +2,26 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .move import *
-from .shipment import *
-from .sale import *
-from .purchase import *
+from . import move
+from . import shipment
+from . import sale
+from . import purchase
 
 
 def register():
     Pool.register(
-        Move,
-        ShipmentIn,
-        ShipmentOut,
-        ShipmentOutReturn,
-        SaleLine,
-        PurchaseLine,
+        move.Move,
+        shipment.ShipmentIn,
+        shipment.ShipmentOut,
+        shipment.ShipmentOutReturn,
+        module='stock_move_sequence', type_='model')
+def register():
+    Pool.register(
+        sale.SaleLine,
+        depends=['sale'],
+        module='stock_move_sequence', type_='model')
+def register():
+    Pool.register(
+        purchase.PurchaseLine,
+        depends=['purchase'],
         module='stock_move_sequence', type_='model')
